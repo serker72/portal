880cb777fcdca133e5ad501f9662669e5e5c51be
serg <serg@berserker-ws>
2015-10-14 22:45:24 +0300

    Первая фиксация

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+/* 
+ * 
+ */
+
+?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="header">
+                <div class="header1">
+                    
+                </div><!-- .header1 -->
+                <div class="header2">
+                    <div class="logo"></div>
+                </div><!-- .header2 -->
+                <div class="header3">
+                    
+                </div><!-- .header3 -->
+            </div><!-- .header -->
+        </div><!-- .wrapper -->
+    </body>
+</html>
