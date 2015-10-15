7fadfa1e14bbdb23e955ebb0e2ab860544e8e161
serg <serg@berserker-ws>
2015-10-15 22:36:52 +0300

    Первая фиксация

--- index.php
+++ index.php
@@ -9,15 +9,27 @@
     <head>
         <meta charset="utf-8">
         <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
         <div class="wrapper">
-            <div class="header">
+            <div class="header row-fluid">
                 <div class="header1">
-                    
+                    <div class="span2 header1_1" style="background-color: #942a25;">
+                        <a href="#">Войти на сайт</a>
+                    </div>
+                    <div class="span5 offset5 header1_2" style="background-color: #942a25;">
+                        <a href="#">Екатеринбург</a>
+                        <input type="text" id="" name="" value="" placeholder="Поиск">
+                    </div>
                 </div><!-- .header1 -->
                 <div class="header2">
-                    <div class="logo"></div>
+                    <div class="span4" style="background-color: #942a25;">
+                        <div class="logo"></div>
+                    </div>
+                    <div class="span3" style="background-color: #942a25;"></div>
+                    <div class="span3" style="background-color: #942a25;"></div>
+                    <div class="span2" style="background-color: #942a25;"></div>
                 </div><!-- .header2 -->
                 <div class="header3">
                     
