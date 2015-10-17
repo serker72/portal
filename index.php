b4c6e6bfbe607fde1697c032aa94a1ffd1f7e0b6
serg <serg@berserker-ws>
2015-10-17 21:48:39 +0300

    Первая фиксация

--- index.php
+++ index.php
@@ -15,26 +15,124 @@
         <div class="wrapper">
             <div class="header row-fluid">
                 <div class="header1">
-                    <div class="span2 header1_1" style="background-color: #942a25;">
+                    <div class="span2 header1_1">
                         <a href="#">Войти на сайт</a>
                     </div>
-                    <div class="span5 offset5 header1_2" style="background-color: #942a25;">
-                        <a href="#">Екатеринбург</a>
-                        <input type="text" id="" name="" value="" placeholder="Поиск">
+                    <div class="span5 offset5 header1_2">
+                        <div id="header1_2_1">
+                            <a href="#">Екатеринбург</a>
+                        </div>
+                        <div id="header1_2_2">
+                            <a href="#"><img src="images/down.png" width="8px"></a>
+                        </div>
+                        <div id="header1_2_3">
+                            <input type="text" id="" name="" value="" placeholder="Поиск">
+                        </div>
                     </div>
                 </div><!-- .header1 -->
                 <div class="header2">
-                    <div class="span4" style="background-color: #942a25;">
+                    <div class="span4">
                         <div class="logo"></div>
                     </div>
-                    <div class="span3" style="background-color: #942a25;"></div>
-                    <div class="span3" style="background-color: #942a25;"></div>
-                    <div class="span2" style="background-color: #942a25;"></div>
+                    <div class="span3">
+                        <div id="header2_2_1">
+                            <a href="#">Поиск заказа</a>
+                        </div>
+                        <div id="header2_2_2">
+                            <a href="#">Поиск услуги</a>
+                        </div>
+                        <div id="header2_2_3">
+                            <a href="#">Поиск оборудования</a>
+                        </div>
+                    </div>
+                    <div class="span3">
+                        <div id="header2_3_1">
+                            <a href="#">Регистрация мастера</a>
+                        </div>
+                        <div id="header2_3_2">
+                            <a href="#">Регистрация бригады</a>
+                        </div>
+                        <div id="header2_3_3">
+                            <a href="#">Регистрация компании</a>
+                        </div>
+                    </div>
+                    <div class="span2">
+                        <div id="header2_4_1">
+                            <a href="#"><img src="images/h2_button1.png"></a>
+                        </div>
+                        <div id="header2_4_2">
+                            <a href="#"><img src="images/h2_button2.png"></a>
+                        </div>
+                    </div>
                 </div><!-- .header2 -->
                 <div class="header3">
-                    
+                    <div class="span12 header3_1">
+                        <div id="header3_1_1">
+                            <ul class="mainmenu">
+                                <li><a href="#">Алмазная резка</a></li>
+                                <li><a href="#">Алмазное бурение</a></li>
+                                <li><a href="#">Демонтаж зданий и сооружений</a></li>
+                                <li><a href="#">Проектирование</a></li>
+                                <li><a href="#">Поставщики оборудования</a></li>
+                                <li><a href="#">Форум</a></li>
+                                <li><a href="#">О проекте</a></li>
+                            </ul>
+                        </div>
+                        <div id="header3_1_2">
+                            <ul class="mainmenu">
+                                <li><a href="#">Строительные организации</a></li>
+                                <li><a href="#">Вывоз/утилизация строительного мусора</a></li>
+                                <li><a href="#">Новости</a></li>
+                                <li><a href="#">Тендеры (заказы)</a></li>
+                                <li><a href="#">Продажа/аренда спец оборудования</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div><!-- .header3 -->
+                <div class="header4">
+                    <p>Рекламный баннер</p>
+                </div><!-- .header4 -->
             </div><!-- .header -->
+            
+            <div class="content-wrapper">
+                <div class="content row-fluid">
+                    <div class="span3 content-left" style="background-color: #46a546;">1
+                    </div>
+                    <div class="span6 content-center" style="background-color: #46a546;">2
+                    </div>
+                    <div class="span3 content-right" style="background-color: #46a546;">3
+                    </div>
+                </div><!-- .content -->
+            </div><!-- .content-wrapper -->
+            
+            <div class="footer-wrapper row-fluid">
+                <div class="footer span12">
+                    <div class="footer_1">
+                        <ul class="mainmenu">
+                            <li><a href="#">Алмазная резка</a></li>
+                            <li><a href="#">Алмазное бурение</a></li>
+                            <li><a href="#">Демонтаж зданий и сооружений</a></li>
+                            <li><a href="#">Проектирование</a></li>
+                            <li><a href="#">Поставщики оборудования</a></li>
+                            <li><a href="#">Форум</a></li>
+                            <li><a href="#">О проекте</a></li>
+                        </ul>
+                    </div><!-- .footer_1 -->
+
+                    <div class="footer_2">
+                        <ul class="mainmenu">
+                            <li><a href="#">Строительные организации</a></li>
+                            <li><a href="#">Вывоз/утилизация строительного мусора</a></li>
+                            <li><a href="#">Новости</a></li>
+                            <li><a href="#">Тендеры (заказы)</a></li>
+                            <li><a href="#">Продажа/аренда спец оборудования</a></li>
+                        </ul>
+                    </div><!-- .footer_2 -->
+
+                    <div class="footer_3">&nbsp;&copy;&nbsp;Перестройка,&nbsp;2015&nbsp;г.
+                    </div><!-- .footer_3 -->
+                </div><!-- .footer -->
+            </div><!-- .footer-wrapper -->
         </div><!-- .wrapper -->
     </body>
 </html>
